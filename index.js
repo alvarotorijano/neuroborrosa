abbeb9bb64455581ff17825a699ffadb80916af1
Alvaro@hackTFG <alvarotorijano@usal.es>
2019-10-31 00:59:22 +0100

    added database connection and config file

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 var express = require("express");
 var app = express();
+var DBConfig = require ('./databaseConnection.json')
+const { Client } = require('pg')
+
+const DBClient = new Client(DBConfig.roomTemp);
+
+DBClient.connect()
+	.then((data, err)=>{
+		console.log('Conected to database: '.green)
+		logToFile('Conected to database: '.green)
+	})
+	.catch((data, err) => {
+		console.log(err);
+		console.log(data)
+	})
 
 app.get("/data", (req, res, next) => {
 	console.log("Esta es mi temperatura: ");
-	console.log(req.params.temp);
+	console.log(req.query.temp);
+	console.log(req.query);
  res.json(["Tony","Lisa","Michael","Ginger","Food"]);
 });
 
