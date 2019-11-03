a32f4d534b93125a54013c57ce99421c97af2830
Alvaro@hackTFG <alvarotorijano@usal.es>
2019-11-03 02:09:12 +0100

    added DB creation script

---
+++ DBCreation.sql
@@ -0,0 +1,15 @@
+create table register (
+	heating boolean,
+	roomtemp real,
+
+	isdaytime boolean,
+	humidity real,
+	temp real,
+	windDir real,
+	windSpeed real,
+	UV real,
+	precipitation real,
+	cloudCover real,
+	moment timestamp,
+	primary key (moment)
+);
